c1bb563e52951110f2a537c6eef3dcc54342d0e9
msoler18 <msoler735@gmail.com>
2018-05-12 12:08:17 -0500

    Solve Challenge

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.typing = this.typing.bind(this)
+
+    this.state = {
+      word:""    
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.typing}/>
+        <p className="repeater">{this.state.word}</p>
       </div>
     );
   }
-}
 
+  typing(e){
+    this.setState({
+      word: e.target.value
+    })
+  }
+}
 export default App;
